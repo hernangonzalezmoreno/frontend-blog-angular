66ef64296a17a9275b53e26dbab8f6d773f2e7ec
Hernán González Moreno <hernangonzalezmoreno@gmail.com>
2021-05-07 15:02:25 -0300

    Maquetacion del login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public pageTitle: string;
+  public user: User;
+
+  constructor(
+    private _userService: UserService
+  ){
+    this.pageTitle = 'Login';
+    this.user = new User(1,'','','ROLE_USER','','','','');
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit( form: any ){
+
   }
 
 }
