e82874b5b5e21d1b9219f9d4abcdd484059489ba
Hernán González Moreno <hernangonzalezmoreno@gmail.com>
2021-05-16 10:46:33 -0300

    Implementacion del UserService en el componente user-edit

--- src/app/components/user-edit/user-edit.component.ts
+++ src/app/components/user-edit/user-edit.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
-  styleUrls: ['./user-edit.component.css']
+  styleUrls: ['./user-edit.component.css'],
+  providers: [ UserService ]
 })
 export class UserEditComponent implements OnInit {
 
   public pageTitle: string = 'Ajustes';
   public user: User;
 
-  constructor() {
-    this.user = new User();
+  constructor(
+    private _userService: UserService
+  ){
+    this.user = this._userService.getIdentity();
   }
 
   ngOnInit(): void {
