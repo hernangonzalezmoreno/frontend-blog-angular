8d2d32e87bb6717777c363cf356460d7f6061be2
Hernán González Moreno <hernangonzalezmoreno@gmail.com>
2021-05-18 17:22:45 -0300

    Nuevo servicio: CategoryService

---
+++ src/app/services/category.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Category } from '../models/category';
+import { global } from './global';
+
+@Injectable()
+export class CategoryService{
+
+  private _url : string;
+
+  constructor(
+    private _http: HttpClient
+  ){
+    this._url = global.url;
+  }
+
+  getHeaders(): HttpHeaders{
+    return new HttpHeaders().set( 'Content-Type', 'application/x-www-form-urlencoded' );
+  }
+
+  create( token: string, category: Category ): Observable<any>{
+    let json = JSON.stringify( category );
+    let params = 'json='+json;
+
+    let headers = this.getHeaders().set( 'Authorization', token );
+
+    return this._http.post( this._url+'category', params, {headers: headers} );
+  }
+
+}
