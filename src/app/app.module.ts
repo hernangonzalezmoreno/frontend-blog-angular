5462da190f26f8df7fa516a8e701d6b3a9e737fb
Hernán González Moreno <hernangonzalezmoreno@gmail.com>
2021-05-26 09:45:18 -0300

    Creando componente 'post-edit' para editar las entradas del blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { UserEditComponent } from './components/user-edit/user-edit.component';
 import { CategoryNewComponent } from './components/category-new/category-new.component';
 import { PostNewComponent } from './components/post-new/post-new.component';
 import { PostDetailComponent } from './components/post-detail/post-detail.component';
+import { PostEditComponent } from './components/post-edit/post-edit.component';
 
 @NgModule({
 
@@ -27,7 +28,8 @@ import { PostDetailComponent } from './components/post-detail/post-detail.compon
     UserEditComponent,
     CategoryNewComponent,
     PostNewComponent,
-    PostDetailComponent
+    PostDetailComponent,
+    PostEditComponent
   ],
 
   // Modulos
